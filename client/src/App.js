389f09e27643be658a8656f14f894a6dda375276
Matheus <corvus@DESKTOP-6EGJ8K5.localdomain>
2022-05-29 02:03:01 -0300

    starting projetct

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> CRUD APP </h1>
+      <div>
+input: <input type="text" />
+input: <input type="text" />
+    <button type='button'> Submit </button>
+    </div>
+    </div>
+  );
+}
+
+export default App;
